3c553bcece6a678e5509c02469717ccf961ac636
kohlarnhin <17625752606@163.com>
2024-03-06 21:26:54 +0800

    提交测试

--- spring-cloud-k8s/koh-user/src/main/java/com/koh/KohUserSpringBootApplication.java
+++ spring-cloud-k8s/koh-user/src/main/java/com/koh/KohUserSpringBootApplication.java
@@ -3,9 +3,10 @@ package com.koh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KohUserSpringBootApplication {
 
     public static void main(String[] args) {
--- spring-cloud-k8s/koh-user/src/main/java/com/koh/controller/UserController.java
+++ spring-cloud-k8s/koh-user/src/main/java/com/koh/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.koh.controller;
 
-import com.koh.entity.User;
-import com.koh.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserService userService;
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//    private final UserService userService;
+//
+//    @Autowired
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/{id}")
+//    public User getUserById(@PathVariable Long id) {
+//        return userService.getById(id);
+//    }
 
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userService.getById(id);
+    @GetMapping("/config")
+    public String getConfig() {
+        return datasourceUrl;
     }
 }
