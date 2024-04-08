a7e5ea1816df1cbdb8fb52adbccd69e541b3a8c0
kohlarnhin <17625752606@163.com>
2024-04-08 10:52:09 +0800

    修改log的autoConfiguration

--- auto-async-log/src/main/java/com/koh/log/LogCommonAutoConfiguration.java
+++ auto-async-log/src/main/java/com/koh/log/LogCommonAutoConfiguration.java
@@ -1,13 +1,13 @@
 package com.koh.log;
 
-import com.tedia.apcp.log.properties.LogAsyncThreadPoolProperties;
-import com.tedia.apcp.log.properties.LogProperties;
+import com.koh.log.properties.LogAsyncThreadPoolProperties;
+import com.koh.log.properties.LogProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(value = {"com.tedia.apcp.log"})
+@ComponentScan(value = {"com.koh"})
 @EnableConfigurationProperties({LogAsyncThreadPoolProperties.class, LogProperties.class})
 public class LogCommonAutoConfiguration {
 }
